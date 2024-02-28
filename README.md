b18e3de656b4900a2abbdb549a108fe6629d627a
ZTStew <zt.stewart.5@gmail.com>
2024-02-28 11:31:31 -0500

    Template 1

---
+++ README.md
@@ -0,0 +1,11 @@
+# Program Name
+---
+## Description:  
+Program description
+
+---
+### Usage:
+(explanation of how to use the program)
+1. Program Steps
+2. Program Steps
+3. Program Steps
---
+++ main.py
@@ -0,0 +1,37 @@
+"""
+Description:
+"""
+
+import os, argparse
+import logging as log
+
+path = os.path.dirname(os.path.abspath(__file__)) + '\\Log\\template.log'
+
+log.basicConfig(
+    filename= path,
+    level=log.DEBUG,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
+# log.debug("debug message")
+# log.info("info message")
+# log.warning("warning message")
+# log.error("error message")
+# log.critical("critical message")
+
+log.critical("### ### ### V Program Starts V ### ### ###")
+
+args = argparse.ArgumentParser()
+args.add_argument(
+  "-t",
+  "--test",
+  type=int,
+  help="Declair if the application should run in test mode [0 -> production (default) | 1 -> test mode]."
+)
+
+args = args.parse_args()
+
+if args.test:
+  log.critical("Test Running")
+
+
+log.critical("Program Terminated")
